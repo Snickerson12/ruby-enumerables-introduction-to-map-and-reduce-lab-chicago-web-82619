9a370f721855673abd3a54aa5de42a20856c70e1
Laura Nickerson <laura.nickerson@gmail.com>
2019-08-15 16:50:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,14 @@ def reduce_to_total(source_array, starting_point)
   return value
 end
   
-  
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
   
   
   
